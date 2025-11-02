eb8a86336363385af2ef98e6ff53ad5481a6c878
baohc1705 <devblue404@gmail.com>
2025-11-02 13:54:04 +0700

    Thêm mới layout theo fragment và replace và cài đặt TailwindCss

--- src/main/java/com/baoduy/UniBook/UniBookApplication.java
+++ src/main/java/com/baoduy/UniBook/UniBookApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UniBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UniBookApplication.class, args);
-	}
-
+        SpringApplication.run(UniBookApplication.class, args);
+        System.out.println("===========================================");
+        System.out.println("Ứng dụng đã khởi động thành công!");
+        System.out.println("Đường dẫn: http://localhost:8080/unibook");
+        System.out.println("===========================================");
+    }
 }
